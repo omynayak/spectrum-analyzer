aa4e450690537f865c4b275b5aab7ba8c0f6a522
omynayak <manelomnayak099@gmail.com>
2026-01-04 09:53:56 +0530

    Added Kaiser Window

--- cpp_files/window.cpp
+++ cpp_files/window.cpp
@@ -3,6 +3,7 @@
 namespace spect_an
 {
     std::vector<double> hamming_window(const std::vector<double>& input){
+        if(input.size() < 2) return input;
         const double two_pi_N = 2.0 * M_PI / (input.size() - 1); 
         std::vector<double> output(input);
         for(size_t i{}; i < output.size(); i++) output[i] *= (0.54 - 0.46 * std::cos(two_pi_N * i));
@@ -10,9 +11,26 @@ namespace spect_an
     }
 
     std::vector<double> hann_window(const std::vector<double>& input){
+        if(input.size() < 2) return input;
         const double two_pi_N = 2.0 * M_PI / (input.size() - 1);
         std::vector<double> output(input);
         for(size_t i{}; i < output.size(); i++) output[i] *= (0.5 - 0.5 * std::cos(two_pi_N * i));
+        return output;
+    }
+
+    std::vector<double> kaiser_window(const std::vector<double>& input, const double beta){
+        if(input.size() < 2) return input;
+        
+        const size_t N = input.size();
+        std::vector<double> output(input);
+
+        double denominator = std::cyl_bessel_i(0, beta);
+
+        for (size_t n = 0; n < N; ++n) {
+            double r = 2.0 * n / (N - 1) - 1.0;
+            double w = std::cyl_bessel_i(0, beta * std::sqrt(1 - r * r)) / denominator;
+            output[n] *= w;
+        }
         return output;
     }
 } // namespace spect_an
